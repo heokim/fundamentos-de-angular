85eb3aa73831a18d4f1771dc0a8296e94d142401
heokim <57774541+heokim@users.noreply.github.com>
2023-06-27 11:39:47 -0400

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+// importando modelo de Poduct
+import { Product } from './product.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -19,6 +22,48 @@ export class AppComponent {
       'https://images.freeimages.com/images/large-previews/f98/black-coffee-1185883.jpg',
   };
 
+  // names: string[]  | number[] | any[]
+  names: string[] = ['Andres', 'Moises', 'Isaac', 'Gloria'];
+  newName: string = '';
+  productos: Product[] = [
+    {
+      name: 'producto 1',
+      price: 10000,
+      image: './assets/images/p1.jpg',
+      category: 'Varios',
+    },
+    {
+      name: 'producto 2',
+      price: 20000,
+      image: './assets/images/p2.jpg',
+    },
+    {
+      name: 'producto 3',
+      price: 30000,
+      image: './assets/images/p3.jpg',
+    },
+    {
+      name: 'producto 4',
+      price: 40000,
+      image: './assets/images/p4.jpg',
+    },
+    {
+      name: 'producto 5',
+      price: 50000,
+      image: './assets/images/p5.jpg',
+    },
+    {
+      name: 'producto 6',
+      price: 60000,
+      image: './assets/images/p6.png',
+    },
+    {
+      name: 'producto 7',
+      price: 70000,
+      image: './assets/images/p7.jpg',
+    },
+  ];
+
   toggleButton() {
     this.btnDisable = !this.btnDisable;
   }
@@ -35,5 +80,14 @@ export class AppComponent {
   changeName(event: Event) {
     const element = event.target as HTMLInputElement;
     this.persona.name = element.value;
+  }
+
+  addName() {
+    this.names.push(this.newName);
+    this.newName = '';
+  }
+
+  eliminarNombre(index: number) {
+    this.names.splice(index, 1);
   }
 }
---
+++ src/app/product.model.ts
@@ -0,0 +1,6 @@
+export interface Product {
+  name: string;
+  price: number;
+  image: string;
+  category?: string; //  ?: es para que el artibuto pueda se opcional
+}
