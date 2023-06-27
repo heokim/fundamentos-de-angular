a79f526cd7e5f09090fa5f642bdbdb2c953af80c
heokim <57774541+heokim@users.noreply.github.com>
2023-06-27 10:44:16 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Andres';
+  age = 24;
+  img =
+    'https://images.freeimages.com/images/large-previews/f98/black-coffee-1185883.jpg';
+  btnDisable = true;
+
+  persona = {
+    name: 'Andres',
+    age: 24,
+    avatar:
+      'https://images.freeimages.com/images/large-previews/f98/black-coffee-1185883.jpg',
+  };
+
+  toggleButton() {
+    this.btnDisable = !this.btnDisable;
+  }
+
+  increaseAge() {
+    this.persona.age++;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.persona.name = element.value;
+  }
 }
